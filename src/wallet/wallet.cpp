2bf9a7e1ab31a8c18fe5e7ff436d9ea4fa589651
SHenckel <SHenckel@users.noreply.github.com>
2015-08-18 22:19:47 +0200

    Testing a commit
    Just a test

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -2,6 +2,7 @@
 // Copyright (c) 2009-2014 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// testing how a commit works
 
 #include "wallet/wallet.h"
 
